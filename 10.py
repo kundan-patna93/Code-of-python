f5fc8c2b507c0668b9b115faca31d7a880d930f5
kundan kumar <kundan.patna93@gmail.com>
2019-07-14 18:59:32 +0530

    Delete 10.py

--- 10.py
+++
@@ -1,6 +0,0 @@
-ch=(input("enter charachter: "))
-res="Vowel" if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U') else "Consonent"
-print(res);
-
-
-
