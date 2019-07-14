4d9bfb0dbc3fdfa355a70495e0af879f7eff206e
kundan kumar <kundan.patna93@gmail.com>
2019-07-14 23:16:35 +0530

    Update 10..py

--- 10..py
+++ 10..py
@@ -1,1 +1,6 @@
-a
+ch=(input("enter charachter: "))
+res="Vowel" if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U') else "Consonent"
+print(res);
+
+
+
