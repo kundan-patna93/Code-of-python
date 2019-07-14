04cc15eb77137979a33b91544e55ed4bbcfd9c34
kundan kumar <kundan.patna93@gmail.com>
2019-07-14 19:00:19 +0530

    Delete 4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-"To get the ASCII code of a character, use the ord() function
-
-ord('a');
-
-"To get the character encoded by an ASCII code number, use the chr() function
-
-chr(95)
-
-both are write shill editor
