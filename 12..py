30ab58b9eb3029b4e3995487980c0f9c48a6e8f0
kundan kumar <kundan.patna93@gmail.com>
2019-07-14 23:17:16 +0530

    Update 12..py

--- 12..py
+++ 12..py
@@ -1,1 +1,9 @@
-a
+"""type1"""
+num=int(input("enter Number: "))
+num2=int(input("enter second no: "))
+num3=int(input("enter third no: "))
+res="first is greater" if (num>num2 and num>num3) else ("num2 is greater" if (num2>num3) else "num3 is greaer")
+print(res);
+
+
+
