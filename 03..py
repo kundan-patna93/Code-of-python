0e678abc3c65fb9b1d932a40ba4997b7fb9550f6
kundan kumar <kundan.patna93@gmail.com>
2019-08-10 01:40:37 +0530

    Update 03..py

--- 03..py
+++ 03..py
@@ -1,1 +1,2 @@
-print((float(input("enter no: "))) * (float(input("enter second no: "))))
+print((float(input("enter no: "))) * (float(input("enter second no: "))))  ''' only for float'''
+print((evl(input("enter no: "))) * (evl(input("enter second no: "))))      ''' apply for both inter and float'''
