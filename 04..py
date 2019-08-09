2265fe2b782c8d80f0ec5778d718a6499f167684
kundan kumar <kundan.patna93@gmail.com>
2019-08-10 01:48:04 +0530

    Update 04..py

--- 04..py
+++ 04..py
@@ -1,9 +1,13 @@
 "To get the ASCII code of a character, use the ord() function
-
 ord('a');
 
 "To get the character encoded by an ASCII code number, use the chr() function
-
 chr(95)
 
-both are write shill editor 
+"""ASCII values character"""
+str1=eval(input("Enter any character: "));
+print(ord(str1));
+
+""" Character to ASCII values"""
+str1=eval(input("Enter any character: "));
+print(chr(str1));
