1021fdb25a68aaccb656e4fcfa25501aae7a9772
kundan kumar <kundan.patna93@gmail.com>
2019-08-12 21:48:29 +0530

    Update 13..py

--- 13..py
+++ 13..py
@@ -1,3 +1,2 @@
 year=int(input("Enter year: "))
-res="leap year" if(year%4==0) else "not leap year"
-print(res)
+print("leap year" if((year%4==0 and year%100!=0) or(year%400==0)) else "Not leap year")
