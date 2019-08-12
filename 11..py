4bb01db234255d3675f087f082cfa16f992c55f0
kundan kumar <kundan.patna93@gmail.com>
2019-08-12 21:43:31 +0530

    Update 11..py

--- 11..py
+++ 11..py
@@ -1,1 +1,5 @@
-a
+num1=int(input("enter first no: "))
+num2=int(input("enter second no: "))
+num3=int(input("enter third no: "))
+num4=int(input("enter fouth no: "))
+print("num1 Greater no" if(num1>num2 and num1>num3 and num1>num4) else "num2 Greater no" if(num2>num3 and num2>num4) else "num3 Greater no" if(num3>num4) else "num4 Greater no");
