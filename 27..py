6b7e5c5a2dfb55cb39f418fe70c6007d624c914d
kundan kumar <kundan.patna93@gmail.com>
2019-08-13 21:09:56 +0530

    Update 27..py

--- 27..py
+++ 27..py
@@ -1,1 +1,11 @@
-a
+num1=int(input("enter Initialization no: "));
+num2=int(input("enter last no: "));
+c=0;
+for i in range(num1+1,num2):
+    for j in range(1,i):
+        if(i%j==0):
+            c=c+1;
+        if(c==2):
+            print(j);
+            break;
+    
