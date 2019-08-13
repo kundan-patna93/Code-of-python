542311fbcfb405b7a98b1fa90cc1c840593d08da
kundan kumar <kundan.patna93@gmail.com>
2019-08-13 21:11:54 +0530

    Update 29..py

--- 29..py
+++ 29..py
@@ -1,1 +1,13 @@
-a
+num=int(input("enter no: "))
+num1=num;
+num2=num;
+c=0;
+res=0;
+while(num>0):
+    num=num//10
+    c=c+1;
+while(num1>0):
+    rem=num1%10;
+    res+=(pow(rem,c));
+    num1=num1//10;
+print(("No is Armstrong",num2,res) if(num2==res) else ("N0 is not armstrong",num2,res))
