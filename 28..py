b1a3ed00f36363479b5c83a6be6f0b614bf04326
kundan kumar <kundan.patna93@gmail.com>
2019-08-13 21:10:59 +0530

    Update 28..py

--- 28..py
+++ 28..py
@@ -1,1 +1,12 @@
-a
+n1=int(input("enter starting no: "))
+n2=int(input("enter ending no: "))
+for i in range(n1,n2):
+    c=0;
+    for j in range(1,i):
+        if(i%j==0):
+            c=c+1;
+    if(c>=2):
+       pass;
+    else:
+       print(i)
+ 
