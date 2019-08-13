f6ef3b24e81738b360194f2a81741616ee2b043f
kundan kumar <kundan.patna93@gmail.com>
2019-08-13 21:08:00 +0530

    Update 25..py

--- 25..py
+++ 25..py
@@ -1,1 +1,11 @@
-a
+num1=int(input("enter base no: "));
+num2=int(input("enter power of no: "));
+'''using inbulid function'''
+print("using inbulide function",pow(num1,num2));
+
+''' without using inculid funcion'''
+cal=1;
+for i in range(1,num2+1):
+    cal=cal*num1;
+print("without using inbulide funcion",cal)
+ 
