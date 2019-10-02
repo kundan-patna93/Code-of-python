1ebaae51c43069ae28158d20d1e606d7b2a8b030
kundan kumar <kundan.patna93@gmail.com>
2019-10-02 22:16:19 +0530

    Update 17..py

--- 17..py
+++ 17..py
@@ -1,5 +1,14 @@
+"""By using For loop"""
 num=int(input("Enter first No: "))
 fact=1;
 for i in range(num,1,-1):
     fact=fact*i;
+print(fact)
+
+"""By using while loop"""
+num=int(input("Enter first No: "))
+fact=1;
+while(num>0):
+    fact=fact*num;
+    num=num-1;
 print(fact)
