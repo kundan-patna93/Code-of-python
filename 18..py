361b56f34f556cadd04d019b4b782818cc9ce382
kundan kumar <kundan.patna93@gmail.com>
2019-10-03 12:19:50 +0530

    Update 18..py

--- 18..py
+++ 18..py
@@ -1,5 +1,13 @@
+"""using for loop"""
 num=int(input("Enter No: "))
 res=0;
 for i in range(1,10+1,1):
     res=num*i;
     print(num,"*",i,"=",res)
+
+    
+ """ while loop"""
+i=1
+while(i<=10):
+    print(i,"*",num,"=",i*n)
+    i=i+1;
