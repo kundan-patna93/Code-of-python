1641a9c9f94a7b52d34723e84a4bb3e0508d8d4a
kundan kumar <kundan.patna93@gmail.com>
2019-10-03 10:02:25 +0530

    Update 04..py

--- 04..py
+++ 04..py
@@ -4,6 +4,8 @@ ord('a');
 "To get the character encoded by an ASCII code number, use the chr() function
 chr(95)
 
+
+"""Method-1"""
 """character to ASCII values"""
 str1=eval(input("Enter any character: "));
 print(ord(str1));
@@ -11,3 +13,9 @@ print(ord(str1));
 """ ASCII values to Character"""
 str1=eval(input("Enter any ASCII: "));
 print(chr(str1));
+
+"""Method-2"""
+print(ord(eval(input("enter character: "))));
+print(chr(eval(input("Entet number: "))));
+
+
