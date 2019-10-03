06c6757179b22596736541ae00fcd4ab07916302
kundan kumar <kundan.patna93@gmail.com>
2019-10-03 12:16:59 +0530

    Update 16..py

--- 16..py
+++ 16..py
@@ -1,5 +1,13 @@
+"""using for loop"""
 num2=int(input("Enter last natural No: "))
 sum=0;
 for i in range(1,num2+1):
     sum=sum+i;
 print(sum)
+
+"""using while loop"""
+sum=0
+while(num>0):
+    sum=sum+num
+    num=num-1
+print("sum of natural no: ",sum)
