fef523b427c7744c64b08649f12519045c68f0c3
kundan kumar <kundan.patna93@gmail.com>
2019-10-03 12:04:45 +0530

    Update 05..py

--- 05..py
+++ 05..py
@@ -2,3 +2,8 @@ n=int(input("enter firstno: "))
 n2=int(input("enter second no: "))
 print("Quotient-",n//n2)
 print("Remainder-",n%n2)
+
+
+"""type 2"""
+print("Quotient: ",int(input("entet fist no: ")) // int(input("enter seocn no: ")));
+print("Reminder: ",int(input("entet fist no: ")) % int(input("enter seocn no: ")));
