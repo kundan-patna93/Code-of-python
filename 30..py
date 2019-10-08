ca27f25edd6e64155f970ce872e0a4e05f7de8a3
kundan kumar <kundan.patna93@gmail.com>
2019-10-08 15:00:48 +0530

    Update 30..py

--- 30..py
+++ 30..py
@@ -1,1 +1,10 @@
-a
+num1=int(input("enter start point no: "))
+num2=int(input("enter ending point no: "))
+for i in range(num1+1,num2):
+    cp,arm=i,0;
+    while(i>0):
+        rem=i%10;
+        arm=(arm+(pow(rem,3)))
+        i=i//10;
+    if(cp==arm):
+        print(arm)
