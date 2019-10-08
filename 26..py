5c2b0b16e0f29d069ff25429b63e1e8fda36e670
kundan kumar <kundan.patna93@gmail.com>
2019-10-08 15:06:49 +0530

    Update 26..py

--- 26..py
+++ 26..py
@@ -1,8 +1,8 @@
-num1=int(input("enter no: "));
+num=int(input("Enter no: "));
 rev=0;
-num=num1;
-while(num1>0):
-    rem=num1%10;
-    rev=rev*10+rem;
-    num1=num1//10;
-print("No is palindrome" if(num==rev) else "No is Not palindrom")
+cp=num;
+while(num>0):
+    rem=num%10;
+    num=num//10;
+    rev=(rev*10)+rem;
+print("Pallindrom no" if(rev==cp) else "Not pallindrom no")
