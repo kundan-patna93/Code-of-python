115a0f743bc9a38486cbcb911df73461cc72e80e
kundan kumar <kundan.patna93@gmail.com>
2020-04-24 16:59:52 +0530

    Update 27..py

--- 27..py
+++ 27..py
@@ -1,11 +1,7 @@
-num1=int(input("enter Initialization no: "));
-num2=int(input("enter last no: "));
-c=0;
-for i in range(num1+1,num2):
-    for j in range(1,i):
-        if(i%j==0):
-            c=c+1;
-        if(c==2):
-            print(j);
-            break;
-    
+
+no=int(input("enter no:"))
+c=0
+for i in range(1,(no//2)):
+    if(no%i==0):
+        c=c+1;
+print("not palindrm" if(c>=2) else "palindrom")
