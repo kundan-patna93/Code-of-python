f4e6996baaf4c67843885760ededd64aa4f6a9a3
kundan kumar <kundan.patna93@gmail.com>
2021-08-01 20:33:59 +0530

    Update 25..py

--- 25..py
+++ 25..py
@@ -4,8 +4,8 @@ num2=int(input("enter power of no: "));
 print("using inbulide function",pow(num1,num2));
 
 ''' without using inculid funcion'''
-cal=1;
+"""cal=1;
 for i in range(1,num2+1):
     cal=cal*num1;
-print("without using inbulide funcion",cal)
+print("without using inbulide funcion",cal)""" once again
  
