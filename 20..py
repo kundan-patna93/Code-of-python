88168882ccc5753ed5ea1e3ff6c53fd3ddc75c31
kundan kumar <kundan.patna93@gmail.com>
2021-08-01 19:00:49 +0530

    Update 20..py

--- 20..py
+++ 20..py
@@ -1,6 +1,7 @@
 n1=int(input("enter first no: "))
 n2=int(input("enter second no: "))
-for i in range(1,n1&n2):
+m=(min(n1,n2))
+for i in range(1,m+1):
     if(n1%i==0 and n2%i==0):
         gcd=i;
 print(gcd);
