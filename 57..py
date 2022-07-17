3051d263656fcbc802bf60f36d746ffd3f9a1ad1
kundan kumar <kundan.patna93@gmail.com>
2022-07-17 21:38:53 +0530

    Update 57..py

--- 57..py
+++ 57..py
@@ -1,1 +1,21 @@
-a
+#Program to Find the Length of a String.
+def find_length_string(s):
+    print("Lenght of string: ",len(s))
+    
+s="Program to Find the Length of a String."
+find_length_string(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
