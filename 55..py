48f80e71f582e1858fb0960aa8406a2db11a5b7f
kundan kumar <kundan.patna93@gmail.com>
2022-07-17 21:37:32 +0530

    Update 55..py

--- 55..py
+++ 55..py
@@ -1,1 +1,36 @@
-a
+#Program to Find the Frequency of Characters in a String.
+""" 
+def find_frequency_char(str1):
+    dic={}
+    str1=str1.replace(" ","")
+    str1=str1.replace(".","")
+    for ch in str1:
+        dic[ch]=dic.get(ch,0)+1
+    print(dic)
+        
+str1="Program to Find the Frequency of Characters in a String."
+find_frequency_char(str1)
+"""
+
+def find_frequency_char(str1):
+    str1=str1.replace(" ","")
+    str1=str1.replace(".","")
+    for ch in str1:
+        c=str1.count(ch)
+        print(ch,"occurence",c)
+        
+str1="Program to Find the Frequency of Characters in a String."
+find_frequency_char(str1)
+
+
+
+
+
+
+
+
+
+
+
+
+
