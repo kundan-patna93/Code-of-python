a55562b7e67d5d0b74a6d3ee30a97ecf53166a9f
kundan kumar <kundan.patna93@gmail.com>
2022-07-17 21:35:09 +0530

    Update 68..py

--- 68..py
+++ 68..py
@@ -1,1 +1,8 @@
-a
+#Program to Write a Sentence to a File
+
+def write_sentence(str1):
+    with open("file.py",'w') as file:
+        data=file.writelines(str1)
+    
+str1="This is python code"
+write_sentence(str1)
