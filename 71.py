6a086673e36f4e4f27beb8fb8331d8d88bfddc6e
kundan kumar <kundan.patna93@gmail.com>
2022-07-17 21:34:00 +0530

    Update 71.py

--- 71.py
+++ 71.py
@@ -1,1 +1,10 @@
-a
+def print_pyramid_pattern(num):
+    for i in range(num):
+        for j in range(num,i,-1):
+            print(" ",end='')
+        for k in range(i+1):
+            print("*",end=" ")
+        print()
+
+num=int(input("Enter No: "))
+print_pyramid_pattern(num)
