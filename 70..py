e3231667ac5401fa400238475c086310fcb80b39
kundan kumar <kundan.patna93@gmail.com>
2022-07-17 21:36:20 +0530

    Update 70..py

--- 70..py
+++ 70..py
@@ -1,1 +1,8 @@
-a
+#Program to Display its own Source Code as Output
+def print_source_code():
+    with open("Code-70.py",'r') as file:
+        data=file.readlines()
+        for val in data:
+            print(val)
+
+print_source_code()
