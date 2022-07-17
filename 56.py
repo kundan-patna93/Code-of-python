ad1f749a8b550e67490454a90a6e220f97b14faf
kundan kumar <kundan.patna93@gmail.com>
2022-07-17 21:38:24 +0530

    Update 56.py

--- 56.py
+++ 56.py
@@ -1,1 +1,31 @@
-a
+#Program to Count the Number of Vowels, Consonants and so on.
+
+def find_number_vol():
+    s='aeiouAEIOU'
+    vol=[]
+    con=[]
+    for ch in str1:
+        if ch in s:
+            vol.append(ch)
+        else:
+            con.append(ch)
+    print("no of vowel not repeted: ",len(set(vol)))
+    print("no of consonet not repeated: ",len(set(con)))
+str1="Program to Count the Number of Vowels, Consonants and so on."
+find_number_vol()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
