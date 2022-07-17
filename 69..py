064ab57f65b4a170aa4376042fa14a7a93f3fa1b
kundan kumar <kundan.patna93@gmail.com>
2022-07-17 21:35:34 +0530

    Update 69..py

--- 69..py
+++ 69..py
@@ -1,1 +1,7 @@
-a
+#Program to Read a Line From a File and Display it
+def print_source_code():
+    with open("Code-70.py",'r') as file:
+        data=file.readline()
+        print(data)
+
+print_source_code()
