b5b1c95d110b7b05cbacbcb0564d0fbbb7dd5cb6
kundan kumar <kundan.patna93@gmail.com>
2022-07-17 21:39:42 +0530

    Update 59..py

--- 59..py
+++ 59..py
@@ -1,1 +1,23 @@
-a
+#Program to Copy String Without using inbulide method.
+def copy_string(s):
+    s1=""
+    for ch in s:
+        s1=s1+ch
+    print(s1)
+s="Program to Copy String Without using inbulide method."
+copy_string(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
